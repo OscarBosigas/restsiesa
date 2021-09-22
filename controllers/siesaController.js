d3841d6454c129ce1c35be47e4182c1ff9cac781
Oscar Bosigas <oscar7bosigas@gmail.com>
2021-09-21 19:17:47 -0500

    .
    init

--- controllers/siesaController.js
+++ controllers/siesaController.js
@@ -4,7 +4,7 @@ var Siesa = require('../models/siesa');
 
 
 exports.lista_mascotas = function (req, res) {
-    Estudiante.estudiantes(function (err, siesa) {
+    Siesa.lista_mascotas(function (err, siesa) {
         if (err)
             res.send(err);
         res.send(siesa);
